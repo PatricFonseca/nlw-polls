ecfb01bf11f7464b682f3dcd0a7085e2e386c8e3
patricfonseca <patric.ccomp@gmail.com>
2024-02-08 23:44:42 -0300

    update routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,26 +1,17 @@
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+// import { PrismaClient } from "@prisma/client";
+// import { z } from "zod";
 
 const app = fastify();
-const prisma = new PrismaClient();
 
-app.post("/polls", async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string(),
-    });
-
-    const { title } = createPollBody.parse(request.body);
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        },
-    });
-
-    return reply.status(201).send({ pollId: poll.id });
-});
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+// const prisma = new PrismaClient();
 
 app.listen({ port: 3000 }).then(() => {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 });
